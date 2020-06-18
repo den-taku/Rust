2f39f4218afee7453f8315fccbb526d71fbd4adf
Tada <sevensins0410s@icloud.com>
2020-06-19 08:25:11 +0900

    eucledean commandline

--- Chapter2/eucledean/src/main.rs
+++ Chapter2/eucledean/src/main.rs
@@ -1,4 +1,28 @@
-fc gcd(mut n: u64, mut m: u64) -> u64 {
+use std::io::Write;
+use std::str::FromStr;
+
+fn main() {
+    let mut numbers = Vec::new();
+
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg)
+                     .expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
+        std::process::exit(1);
+    }
+ 
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The gratest common divisor of {:?} is {}",numbers, d);
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m != 0);
     while m != 0{
         if m < n {
@@ -6,10 +30,17 @@ fc gcd(mut n: u64, mut m: u64) -> u64 {
             m = n;
             n = t;
         }
+        m = m % n;
     }
     n
 }
 
-fn main() {
-    println!("Hello, world!");
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17,
+                   3 * 7 * 11 * 13 * 19),
+               3 * 11);
 }
+
+
