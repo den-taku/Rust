c488ac548132d0717aeeee14a37c2302edb7cde4
Tada <sevensins0410s@icloud.com>
2020-06-24 22:16:05 +0900

    Chapter2 Finish ~2.6.8

--- Chapter2/mandelbrot/src/main.rs
+++ Chapter2/mandelbrot/src/main.rs
@@ -1,28 +1,75 @@
 extern crate num;
 extern crate image;
+extern crate crossbeam;
 
 use num::Complex;
 use std::str::FromStr;
 use image::ColorType;
 use image::png::PNGEncoder;
 use std::fs::File;
+use std::io::Write;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    
+    if args.len() != 5 {
+        writeln!(std::io::stderr(),
+                 "Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERRRIGHT")
+            .unwrap();
+        writeln!(std::io::stderr(),
+                 "Example: {}mandel.png 1000x750 -1.20, 0.35 -1,0.20",
+                  args[0])
+           .unwrap();
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x')
+        .expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3])
+        .expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4])
+        .expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    // render(&mut pixels, bounds, upper_left, lower_right);
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+
+    {
+        let bands: Vec<&mut [u8]> =
+            pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+
+                spawner.spawn(move || {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        });
+    }
+
+    write_image(&args[1], &pixels, bounds)
+        .expect("error writing PNG file");
 }
 
-fn square_loop(mut x: f64) {
-    loop{
-        x = x * x;
-    }
-}
+// fn square_loop(mut x: f64) {
+//     loop{
+//         x = x * x;
+//     }
+// }
 
-fn square_add_loop(c: f64) {
-    let mut x = 0.;
-    loop {
-        x = x * x + c;
-    }
-}
+// fn square_add_loop(c: f64) {
+//     let mut x = 0.;
+//     loop {
+//         x = x * x + c;
+//     }
+// }
 
 #[allow(dead_code)]
 fn complex_square_add_loop(c: Complex<f64>) {
@@ -45,7 +92,7 @@ fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     
     for i in 0..limit {
         z = z * z + c;
-        if z.norm_sqrt() > 4.0 {
+        if z.norm_sqr() > 4.0 {
             return Some(i);
         }
     }
@@ -65,7 +112,7 @@ fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
 
 fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T,T)> {
     match s.find(separator) {
-        None -> None,
+        None => None,
         Some(index) => {
             match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
                 (Ok(l), Ok(r)) => Some((l, r)),
@@ -115,12 +162,12 @@ fn pixel_to_point(bounds: (usize, usize),
     -> Complex<f64>
 {
     let (width, height) = (lower_right.re - upper_left.re,
-                           upper_left.im - lower.right.im);
+                           upper_left.im - lower_right.im);
     Complex {
         re: upper_left.re + pixel.0 as f64 * width  / bounds.0 as f64,
         im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
-        /// Why substraction here? pixel.1 increases as we go down,
-        /// but the imaginary component increases as we go up.
+        // Why substraction here? pixel.1 increases as we go down,
+        // but the imaginary component increases as we go up.
     }
 }
 
@@ -139,18 +186,18 @@ fn test_pixel_to_point() {
 /// arguments specify points on the complexx plane corresponding to the upper-
 /// left and lower-right corners of the pixel buffer.
 fn render(pixels: &mut [u8],
-          bounds: usize, usize),
+          bounds: (usize, usize),
           upper_left: Complex<f64>,
           lower_right: Complex<f64>)
 {
-    assert!(pixel.len() == bounds.0 * bounds.1);
+    assert!(pixels.len() == bounds.0 * bounds.1);
  
     for row in 0 .. bounds.1 {
         for column in 0 .. bounds.0 {
             let point = pixel_to_point(bounds, (column, row),
                                        upper_left, lower_right);
             pixels[row * bounds.0 + column] =
-                match escape_time(point. 255) {
+                match escape_time(point, 255) {
                     None => 0,
                     Some(count) => 255 - count as u8
                 };
@@ -163,11 +210,11 @@ fn render(pixels: &mut [u8],
 fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize))
     -> Result<(), std::io::Error>
 {
-    let output = File::crate(filename)?;
+    let output = File::create(filename)?;
     
     let encoder = PNGEncoder::new(output);
     encoder.encode(&pixels,
-                   bounds.0 as u32, bounds.1 as u 32,
+                   bounds.0 as u32, bounds.1 as u32,
                    ColorType::Gray(8))?;
     Ok(())
 }
