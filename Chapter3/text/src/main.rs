f46111ba461a79a6cf938b3ef681f8798937ac1c
Tada <sevensins0410s@icloud.com>
2020-06-26 08:40:41 +0900

    Chapter3 ~ 3.4.3

--- Chapter3/text/src/main.rs
+++ Chapter3/text/src/main.rs
@@ -1,8 +1,41 @@
 fn main() {
-    let text = "I see the eigenvalue in thine eye";
-    let (head, tail) = text.split_at(21);
-    println!("{}",head);
-    println!("{}",tail);
-    let t = (12, "eggs");
-    let _b = Box::new(t);
+    // let text = "I see the eigenvalue in thine eye";
+    // let (head, tail) = text.split_at(21);
+    // println!("{}",head);
+    // println!("{}",tail);
+    // let t = (12, "eggs");
+    // let _b = Box::new(t);
+
+    let mut sieve = [true; 10000];
+    for i in 2..100 {
+        if sieve[i] {
+            let mut j = i * i;
+            while j < 10000 {
+                sieve[j] = false;
+                j += i;
+            }
+        }
+    }
+    sieve.sort();
+    // for i in 1..100 {
+    //     println!("{}", sieve[i]);
+    // }
+    // println!("{}", sieve);
+    
+    let mut v = vec![2, 3, 5, 7];
+    v.push(11);
+    v.push(13);
+    println!("{}", v.len());
+    println!("{}", v.capacity());
+    println!("{}", v.iter().fold(1, |a, b| a* b));
+
+    let languages: Vec<String> = std::env::args().skip(1).collect();
+    for l in languages {
+        println!("{}: {}", l,
+                 if l.len() % 2 == 0 {
+                     "functional"
+                 } else {
+                     "imperative"
+                 });
+    }
 }
