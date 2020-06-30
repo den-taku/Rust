71ec9a67e40b416e5b7bc9c1bf67a348fc8f9460
Tada <sevensins0410s@icloud.com>
2020-06-30 20:10:13 +0900

    ~4.2.0

--- Chapter4/ownership/src/main.rs
+++ Chapter4/ownership/src/main.rs
@@ -1,7 +1,16 @@
 fn main() {
+    let s = vec!["udon".to_string(), "ramen".to_string(), "soba".to_string()];
+    let _t = s.clone();
+    let _u = s.clone();
+    let nemoto:f64 = 0.5;
+    let hasaki:f64 = nemoto;
+    let hasa2i:f64 = nemoto;
+    println!("{}", nemoto);
+    println!("{}", hasaki);
+    println!("{}", hasa2i);
     {
         let point = Box::new((0.625, 0.5));
-        let label = format!("{:?}", point);
+        let label = format!("{:?}", point); 
         assert_eq!(label, "(0.625, 0.5)");
     }
     println!("Hello, world!");
