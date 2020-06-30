ec6a001f8855082e3ce2dd61c5023fab88b0efca
Tada <sevensins0410s@icloud.com>
2020-06-30 22:42:09 +0900

    ~4.2

--- Chapter4/ownership/src/main.rs
+++ Chapter4/ownership/src/main.rs
@@ -15,16 +15,38 @@ fn main() {
     }
     println!("Hello, world!");
     
-    struct Person { name: String, birth: i32 }
+    struct Person { name: Option<String>, birth: i32 }
 
     let mut composers = Vec::new();
-    composers.push(Person { name: "Palestrina".to_string(),
+    composers.push(Person { name: Some("Palestrina".to_string()),
                             birth: 1525 });
-    composers.push(Person { name: "Dowland".to_string(),
-                            birth: 1563 });
-    composers.push(Person { name: "Lully".to_string(),
-                            birth: 1632 });
-    for composers in &composers {
-        println!("{}, born {}", composers.name, composers.birth);
+    // let first_name = std::mem::replace(&mut composers[0].name, None);
+    let first_name = composers[0].name.take();
+    assert_eq!(first_name, Some("Palestrina".to_string()));
+    assert_eq!(composers[0].name, None);
+    
+
+    // composers.push(Person { name: "Dowland".to_string(),
+    //                         birth: 1563 });
+    // composers.push(Person { name: "Lully".to_string(),
+    //                         birth: 1632 });
+    // for composers in &composers {
+    //     println!("{}, born {}", composers.name, composers.birth);
+    // }
+
+    let mut v = Vec::new();
+    for i in 101 .. 106 {
+        v.push(i.to_string());
     }
+    
+    let fifth = v.pop().unwrap();
+    assert_eq!(fifth, "105");
+
+    let second = v.swap_remove(1);
+    assert_eq!(second, "102");
+ 
+    let  third = std::mem::replace(&mut v[2], "substitute".to_string());
+    assert_eq!(third, "103");
+
+    assert_eq!(v, vec!["101", "104", "substitute"]);
 }
