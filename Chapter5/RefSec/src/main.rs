dd0fac15cc6913e8e9c4e0b7fde49b4afa461914
Tada <sevensins0410s@icloud.com>
2020-07-02 23:31:26 +0900

    ~5.2.2

--- Chapter5/RefSec/src/main.rs
+++ Chapter5/RefSec/src/main.rs
@@ -1,4 +1,13 @@
+static mut STASH: &i32 = &10;
+static WORTH_POINTING_AT: i32 = 1000;
+fn f(p: &'static i32) { 
+    unsafe {
+        STASH = p;
+    }
+}
+
 fn main() {
+    f(&WORTH_POINTING_AT);
     let x = 1;
     {
         let r = &x;
