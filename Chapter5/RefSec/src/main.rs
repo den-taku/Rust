2946f73fc84ec426b31a4c6cdd9e5d24c28bbb90
Tada <sevensins0410s@icloud.com>
2020-07-04 15:50:34 +0900

    ~5.2.5

--- Chapter5/RefSec/src/main.rs
+++ Chapter5/RefSec/src/main.rs
@@ -1,9 +1,18 @@
 static mut STASH: &i32 = &10;
 static WORTH_POINTING_AT: i32 = 1000;
+
 fn f(p: &'static i32) { 
     unsafe {
         STASH = p;
     }
+}
+
+fn _smallest<'a>(v: &'a [i32]) -> &'a i32 {
+    let mut s = &v[0];
+    for r in &v[1..]{
+        if *r < *s { s = r; }
+    }
+    s
 }
 
 fn main() {
@@ -12,5 +21,19 @@ fn main() {
     {
         let r = &x;
         assert_eq!(*r, 1);
+    }
+
+    struct S<'b>{
+        r: &'b i32
+    }
+
+    {
+        let s;
+        let  x = 10;
+        s = S { r: &x };
+        assert_eq!(*s.r, 10);
+    }
+    struct _T<'hoge> {
+        s: S<'hoge>
     }
 }
