756ffec0a2a1585f1c7e13cb49058df22c5a9419
Tada <sevensins0410s@icloud.com>
2020-07-05 21:17:33 +0900

    ~6.4

--- Chapter6/exp/src/main.rs
+++ Chapter6/exp/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
+fn type_of<T>(_: T) ->String {
+    let a = std::any::type_name::<T>();
+    a.to_string()
+} 
 
+fn main() {
+    let a = { struct S {a: i32, b: i32} };
+    println!("{}", type_of(a));
+    let b = 12f32;
+    let b = b;
+    println!("{}", b);
+    let c = if true {;};
+    println!("{}", type_of(c));
+    let d = if true { 3 } else { 5 };
+    println!("{}", type_of(d));
+
+    let code = 9; 
+    match code {
+        0 => println!("Ok!"),
+        1 => println!("Wires Tangled"),
+        2 => println!("User Sleep"),
+        _ => println!("Unrecognized Error {}", code)
+    }
+ 
+    let g = (let f = 32);
+    println!("{}", type_of(g));
 }
