aaf328b59f3c1ff10770064f1c249848dff8f42b
DenTaku <dentaku1412@gmail.com>
2020-07-08 00:58:19 +0900

    ~8.2

--- Chapter8/module/src/main.rs
+++ Chapter8/module/src/main.rs
@@ -1,6 +1,27 @@
 use std::collections::{HashMap, HashSet};
 use std::io::prelude::*;
 
+mod mimpl {
+    // impl Cell {
+    //     pub fn distande_from_origin(&self) -> f64 {
+    //         f64::hypot(self.x, self.y)
+    //     }
+    // }
+
+    pub const ROOM_TEMPERATURE: f64 = 20.0;
+    pub static ROM_TEMPERATURE: f64 = 20.0;
+}
+
+mod parent {
+    use std::collections::HashMap;
+    mod child {
+        use std::collections::HashMap;
+        struct Num {
+            map: HashMap<i32, i32>
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
