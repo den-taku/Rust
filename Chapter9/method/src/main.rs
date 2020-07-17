03987a59e222090376299ae38c8a60089fc4ee4a
DenTaku <dentaku1412@gmail.com>
2020-07-17 23:58:17 +0900

    ~9.5

---
+++ Chapter9/method/src/main.rs
@@ -0,0 +1,79 @@
+/// A first-in, first-out queue of characters.
+pub struct Queue {
+    older: Vec<char>,  // older elements, eldest last.
+    younger: Vec<char> // younger elements, youngest last.
+}
+
+impl Queue {
+    /// Push a character onto the back of a queue.
+    pub fn push(&mut self, c: char) {
+        self.younger.push(c);
+    }
+
+    /// Pop a character off the front of a queue. Return `Some(c)` if there
+    /// was a character to pop, or `None` if the queue was empty.
+    pub fn pop(&mut self) -> Option<char> {
+        if self.older.is_empty() {
+            if self.younger.is_empty() {
+                return None;
+            }
+
+            // Bring the elements in younger over to older, and put them in 
+            // the promised order.
+            use std::mem::swap;
+            swap(&mut self.older, &mut self.younger);
+            self.older.reverse();
+        }
+
+        // Now older is guaranteed to have something. Vec's pop method
+        // already returns as Option, so we're set.
+        self.older.pop()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.older.is_empty() && self.younger.is_empty()
+    }
+
+    pub fn split(self) -> (Vec<char>, Vec<char>) {
+        (self.older, self.younger)
+    }
+
+    pub fn new() -> Queue {
+        Queue { older: Vec::new(), younger: Vec::new() }
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+    
+    let mut q = Queue { older: Vec::new(), younger: Vec::new() };
+    
+    q.push('0');
+    q.push('1');
+    assert_eq!(q.pop(), Some('0'));
+
+    q.push('∞');
+    assert_eq!(q.pop(), Some('1'));
+    assert_eq!(q.pop(), Some('∞'));
+    assert_eq!(q.pop(), None);
+
+    assert!(q.is_empty());
+    q.push('●');
+    assert!(!q.is_empty());
+
+    let _ = q.pop();
+    assert!(q.is_empty());
+
+    q.push('P');
+    q.push('D');
+    assert_eq!(q.pop(), Some('P'));
+    q.push('X');
+
+    let (older, younger) = q.split();
+    // q is now uninitialized.
+    assert_eq!(older, vec!['D']);
+    assert_eq!(younger, vec!['X']);
+
+    let mut p = Queue::new();
+    p.push('*');
+}
