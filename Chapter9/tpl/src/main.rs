2cf2450757196043db458c2c27b5fe990b69d66e
DenTaku <dentaku1412@gmail.com>
2020-07-19 23:43:20 +0900

    ~9.7

--- Chapter9/tpl/src/main.rs
+++ Chapter9/tpl/src/main.rs
@@ -1,6 +1,24 @@
-struct Bounds(usize, usize);
+struct Extrema<'elt> {
+    greatest: &'elt i32,
+    least: &'elt i32
+}
+
+fn find_extrema<'s>(slice: &'s [i32]) -> Extrema<'s> {
+    let mut greatest = &slice[0];
+    let mut least = &slice[0];
+
+    for i in 1..slice.len() {
+        if slice[i] < *least    { least    = &slice[i]; } 
+        if slice[i] > *greatest { greatest = &slice[i]; }
+    }
+    Extrema { greatest, least }
+}
 
 fn main() {
-    let image_bounds = Bounds(1024, 768)
+    let a = [0, -3, 0, 15, 48];
+    let e = find_extrema(&a);
+    assert_eq!(*e.least, -3);
+    assert_eq!(*e.greatest, 48);
+
     println!("Hello, world!");
 }
