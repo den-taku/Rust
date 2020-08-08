25cffae54ed14594ea23cf2e4ec897d920d822c3
DenTaku <dentaku1412@gmail.com>
2020-08-09 06:52:17 +0900

    ~10.2.8

--- Chapter10/pattern2/src/main.rs
+++ Chapter10/pattern2/src/main.rs
@@ -1,3 +1,35 @@
+// An ordered celloction of 'T's.
+enum BinaryTree<T> {
+    Empty,
+    NonEmpty(Box<TreeNode<T>>)
+}
+
+// A part of a BinaryTee.
+struct TreeNode<T> {
+    element: T,
+    left: BinaryTree<T>,
+    right: BinaryTree<T>
+}
+
+impl<T: Ord> BinaryTree<T> {
+    fn add(&mut self, value: T) {
+        match *self {
+            BinaryTree::Empty =>
+                *self = BinaryTree::NonEmpty(Box::new(TreeNode {
+                    element: value,
+                    left: BinaryTree::Empty,
+                    right: BinaryTree::Empty,
+                })),
+            BinaryTree::NonEmpty(ref mut node) =>
+                if value <= node.element {
+                    node.left.add(value);
+                } else {
+                    node.right.add(value);
+                }
+        }
+    }
+} 
+
 fn main() {
     let int = 2;
     match int {
@@ -14,4 +46,8 @@ fn main() {
         // x @ (a,b) => {} // err: pattern bindings after an `@` are unsttable.
         x @ (..) => { println!("( {}, {} )", x.0, x.1); }
     }
+
+    let mut tree = BinaryTree::Empty;
+    tree.add("Mercury");
+    tree.add("Venus");
 }
