4482a440f685586d034c531924b7bd045edf494d
DenTaku <dentaku1412@gmail.com>
2020-08-08 21:42:50 +0900

    ~10.1.4

--- Chapter10/json/src/main.rs
+++ Chapter10/json/src/main.rs
@@ -9,6 +9,72 @@ enum Json {
     Object(Box<HashMap<String, Json>>)
 }
 
+enum Option<T> {
+    None,
+    Some(T)
+}
+
+enum Result<T, E> {
+    Ok(T),
+    Err(E)
+}
+
+// An ordered celloction of 'T's.
+enum BinaryTree<T> {
+    Empty,
+    NonEmpty(Box<TreeNode<T>>)
+}
+
+// A part of a BinaryTee.
+struct TreeNode<T> {
+    element: T,
+    left: BinaryTree<T>,
+    right: BinaryTree<T>
+}
+
 fn main() {
+    use self::BinaryTree::*;
+
+    let jupiter_tree = NonEmpty(Box::new(TreeNode {
+        element: "Jupiter",
+        left: Empty,
+        right: Empty
+    }));
+
+    let mercury_tree = NonEmpty(Box::new(TreeNode {
+        element: "Mercury",
+        left: Empty,
+        right: Empty
+    }));
+
+    let venus_tree = NonEmpty(Box::new(TreeNode {
+        element: "Venus",
+        left: Empty,
+        right: Empty
+    }));
+
+    let mars_tree = NonEmpty(Box::new(TreeNode {
+        element: "Mars",
+        left: jupiter_tree,
+        right: mercury_tree
+    }));
+
+    let uranus_tree = NonEmpty(Box::new(TreeNode {
+        element: "Uranus",
+        left: Empty,
+        right: venus_tree
+    }));
+
+    let tree = NonEmpty(Box::new(TreeNode {
+        element: "Saturn",
+        left: mars_tree,
+        right: uranus_tree
+    }));
+
+//    let mut tree = BinaryTree::Empty;
+//    for planet in planets {
+//        tree.add(planet);
+//    }    
+
     println!("Hello, world!");
 }
