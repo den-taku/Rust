3937b40fbe167a33926eff37dbb9033d4f6474c6
DenTaku <dentaku1412@gmail.com>
2020-08-10 20:23:09 +0900

    ~11.2.5

---
+++ Chapter11/ss/src/main.rs
@@ -0,0 +1,34 @@
+trait StrignSet {
+    /// Return a new empty set.
+    fn new() -> Self
+        where Self: Sized;
+
+    /// Return a set that contains all the strings in `strings`.
+    fn from_slice(strings: &[&str]) -> Self
+        where Self: Sized;
+
+    /// Find out if this set contains a particular `value`.
+    fn contains(&self, string: &str) -> bool;
+
+    /// Add a string to this set.
+    fn add(&mut self, string: &str);
+}
+
+/// Return the set of words in `document` that aren't in `wordlist`.
+fn unknown_words<S: StringSet>(document: &Vec<String>, wordlist: &S) -> S {
+    let mut unknowns = S::new();
+    for word in document {
+        if !wordlist.contains(word) {
+            unknowns.add(word);
+        }
+    }
+    unknowns
+}
+
+fn main() {
+    // Create sets of two hypothetical types that impl StringSet:
+    let set1 = SortedStrignSet::new();
+    let set2 = HashedStringSet::new();    
+
+    println!("Hello, world!");
+}
