f5e47cbe0c6583eadecc5a21c9d48de5be418d42
DenTaku <dentaku1412@gmail.com>
2020-08-10 21:53:25 +0900

    ~11.4.2

--- Chapter11/method/src/main.rs
+++ Chapter11/method/src/main.rs
@@ -1,3 +1,12 @@
+/// std::ops::Mul, the trait for types that support `*`.
+pub trait Mul<RHS=Self> {
+    /// The resulting type after applying the `*` operator
+    type Output;
+
+    /// The method for the `*` operator
+    fn mul(self, rhs: RHS) -> Self::Output;
+}
+
 fn main() {
     println!("Hage!");
 
