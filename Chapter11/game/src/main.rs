8b60e72a3b6647240e0429d3506d45d0a49f4588
DenTaku <dentaku1412@gmail.com>
2020-08-10 17:04:56 +0900

    ~11.2.0

---
+++ Chapter11/game/src/main.rs
@@ -0,0 +1,36 @@
+/// A trait for characters, items, and scenery -
+/// anything in the game world that's visible on screen.
+trait Visible {
+   /// Render this object on the given canvas.
+   fn draw(&self, canvas: &mut Canvas);
+
+   /// Return true if clicking at (x,y) should
+   /// select this object.
+   fn hit_test(&self, x: i32, y: i32) ->bool;
+}
+
+impl Broom {
+    /// Helper function used by Broom::draw() below.
+    fn broomstick_range(&self) -> Range<i32> {
+        self.y - self.hight - 1 .. self.y
+    }
+}
+
+impl Visible for Broom {
+    fn draw(&self, canvas: &mut Canvas) {
+        for y in self.broomstick_range() {
+            canvas.write_at(self.x, y, '|');
+        }
+        canvas.write_at(self.x, self.y, 'M');
+    } 
+
+    fn hit_test(&self, x: i32, y: i32) -> bool {
+        self.x == x
+        && self.y - self.height - 1 <= y
+        && y <= self.y
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
