ce92d0890b739acfb62cc9216ae3a2d3578606da
DenTaku <dentaku1412@gmail.com>
2020-08-12 03:29:13 +0900

    ~12.1.1

--- Chapter12/ope/src/main.rs
+++ Chapter12/ope/src/main.rs
@@ -1,8 +1,19 @@
-use std::ops::Add;
+use std::ops::{Neg, Add};
 
+#[derive(Copy, Clone)]
 struct Complex<T> {
     re: T,
     im: T
+}
+
+impl<T, O> Neg for Complex<T>
+    where T: Neg<Output=O>
+{
+    type Output = Complex<O>;
+    
+    fn neg(self) -> Complex<O> {
+        Complex{ re: -self.re, im: -self.im }
+    }
 }
 
 impl<L, R, O> Add<Complex<R>> for Complex<L> 
@@ -18,6 +29,14 @@ impl<L, R, O> Add<Complex<R>> for Complex<L>
 fn main() {
     assert_eq!(4.125f32.add(5.75), 9.875);
     assert_eq!(10.add(20), 10 + 20);
+
+    let a = Complex{ re: 3f32, im: 4f32 }; 
+    let b = Complex{ re: 9f32, im: 8f32 }; 
+    println!("( {} + {}i ) + ( {} + {}i ) = {} + {}i", 
+        a.re, a.im, b.re, b.im, 
+        (a + b).re, <Complex<f32> as Add<Complex<f32>>>::add(a, b).im);
+    println!("-({} + {}i) = {} + {}i",
+        a.re, a.im, (-a).re, <Complex<f32> as Neg>::neg(a).im);
 
     println!("Hello, world!");
 }
