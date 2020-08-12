f1bc05494c883f98b640a4c524b71d5cbcea3761
DenTaku <dentaku1412@gmail.com>
2020-08-13 05:15:46 +0900

    ~13.1

---
+++ Chapter13/drop/src/main.rs
@@ -0,0 +1,76 @@
+use std::ops::Drop;
+
+struct Appellation {
+    name: String,
+    nicknames: Vec<String>
+}
+
+impl Drop for Appellation {
+    fn drop(&mut self) {
+        print!("Dropping {}", self.name);
+        if !self.nicknames.is_empty() {
+            print!(" (AKA {})", self.nicknames.join(", "));
+        }
+        println!("");
+    }
+}
+
+fn main() {
+    let mut a = Appellation { name: "Zeus".to_string(),
+                              nicknames: vec!["cloud collector".to_string(),
+                                              "king of the gods".to_string()] };
+    println!("before assignment");
+    a = Appellation { name: "Hera".to_string(), nicknames: vec![] };
+    println!("at end of block");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
