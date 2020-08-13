796376380b9fb69589540c58f009795028923186
DenTaku <dentaku1412@gmail.com>
2020-08-14 05:21:04 +0900

    ~13.11

---
+++ Chapter13/cow/src/main.rs
@@ -0,0 +1,18 @@
+use std::path::PathBuf;
+use std::borrow::Cow;
+
+fn describe(error: &Error) -> Cow<'static, str> {
+    match *error {
+        Error::OutOfMemory   => "out of memory".into(),
+        Error::StackOverflow => "stack overflow".into(),
+        Error::MachineOnFire => "machine on fire".into(),
+        Error::Unfathomable  => "machine bewildered".into(),
+        Error::FileNotFind(ref path) => {
+            format!("file not found: {}", path.display()).into()
+        }
+    }
+}
+
+fn main() {
+    println!("Disaster has struck: {}", describe(&error));
+}
