b7e18880304c3f9632ea880c95e5c31b26e10d40
DenTaku <dentaku1412@gmail.com>
2020-08-15 10:05:14 +0900

    ~14.1.0

--- Chapter14/city/src/main.rs
+++ Chapter14/city/src/main.rs
@@ -17,6 +17,11 @@ fn sort_cities(cities: &mut Vec<City>) {
     cities.sort_by_key(|city| -city.population);
 }
 
+// /// Sort by any of several different statistics.
+// fn sort_by_statistics(cities: &mut Vec<City>, stat: Statistic) {
+//     cities.sort_by_key(|city| -city.get_statistic(stat));
+// }
+
 fn main() {
     println!("Hello, world!");
 }
