3c0a5daa31f2fbbe428d6f8e2993caef75050b19
DenTaku <dentaku1412@gmail.com>
2020-08-15 10:43:34 +0900

    ~14.1.2

--- Chapter14/city/src/main.rs
+++ Chapter14/city/src/main.rs
@@ -1,3 +1,5 @@
+use std::thread;
+
 struct City {
     name: String,
     population: i64,
@@ -17,10 +19,22 @@ fn sort_cities(cities: &mut Vec<City>) {
     cities.sort_by_key(|city| -city.population);
 }
 
-// /// Sort by any of several different statistics.
-// fn sort_by_statistics(cities: &mut Vec<City>, stat: Statistic) {
-//     cities.sort_by_key(|city| -city.get_statistic(stat));
-// }
+/// Sort by any of several different statistics.
+fn sort_by_statistics(cities: &mut Vec<City>, stat: Statistic) {
+    cities.sort_by_key(|city| -city.get_statistic(stat));
+}
+
+fn start_sorting_thread(mut cities: Vec<City>, stat: Statistic)
+    -> thread::JoinHandle<Vec<City>>
+{
+    // let key_fn = |city: &City| -> i64 { -city.get_statistics(stat) };
+    let key_fn = move |city: &City| -> i64 { -city.get_statistic(stat) };
+
+    thread::spawn(move || {
+        cities.sort_by_key(key_fn);
+        cities
+    })
+}
 
 fn main() {
     println!("Hello, world!");
