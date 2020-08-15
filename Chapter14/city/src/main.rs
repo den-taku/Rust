3550e3067ab8bc68678bb98250241ecd40266bcd
DenTaku <dentaku1412@gmail.com>
2020-08-15 15:31:09 +0900

    ~14.2

--- Chapter14/city/src/main.rs
+++ Chapter14/city/src/main.rs
@@ -1,9 +1,10 @@
-use std::thread;
+// use std::thread;
 
 struct City {
     name: String,
     population: i64,
-    country: String
+    country: String,
+    monster_attack_risk: f32
 }
 
 /// Helper function for sorting cities by population.
@@ -19,23 +20,92 @@ fn sort_cities(cities: &mut Vec<City>) {
     cities.sort_by_key(|city| -city.population);
 }
 
-/// Sort by any of several different statistics.
-fn sort_by_statistics(cities: &mut Vec<City>, stat: Statistic) {
-    cities.sort_by_key(|city| -city.get_statistic(stat));
+// /// Sort by any of several different statistics.
+// fn sort_by_statistics(cities: &mut Vec<City>, stat: Statistic) {
+//     cities.sort_by_key(|city| -city.get_statistic(stat));
+// }
+
+// fn start_sorting_thread(mut cities: Vec<City>, stat: Statistic)
+//     -> thread::JoinHandle<Vec<City>>
+// {
+//     // let key_fn = |city: &City| -> i64 { -city.get_statistics(stat) };
+//     let key_fn = move |city: &City| -> i64 { -city.get_statistic(stat) };
+// 
+//     thread::spawn(move || {
+//         cities.sort_by_key(key_fn);
+//         cities
+//     })
+// }
+
+/// Given a list of cities and a test function,
+/// return how many cities pass the test.
+fn count_selected_cities<F>(cities: &Vec<City>, test_fn: F) -> usize 
+where F: Fn(&City) -> bool
+{
+    let mut count = 0;
+    for city in cities {
+        if test_fn(city) {
+            count += 1;
+        }
+    }
+    count
 }
 
-fn start_sorting_thread(mut cities: Vec<City>, stat: Statistic)
-    -> thread::JoinHandle<Vec<City>>
-{
-    // let key_fn = |city: &City| -> i64 { -city.get_statistics(stat) };
-    let key_fn = move |city: &City| -> i64 { -city.get_statistic(stat) };
-
-    thread::spawn(move || {
-        cities.sort_by_key(key_fn);
-        cities
-    })
+/// An example of a test function. Note that the type of 
+/// this function is `fn(&City) -> bool`, the same as
+/// the `test_fn` argument to `count_selected_cities`.
+fn has_monster_attacks(city: &City) -> bool {
+    city.monster_attack_risk > 0.0
 }
 
 fn main() {
-    println!("Hello, world!");
+    // let my_key_fn: fn(&City) -> i64 =
+    //     if user.prefs.by_population {
+    //         city_population_descending
+    //     } else {
+    //         city_monster_attack_risk_descending
+    //     };
+    //
+    // cities.sort_by_key(my_key_fn);
+
+    // How many cities are at risk for monster attack?
+    let my_cities = vec![];
+    let n = count_selected_cities(&my_cities, has_monster_attacks);
+    println!("{} cities has at risk for monster attack.", n);
+
+    let limit = 0.0;
+    let n = count_selected_cities(
+        &my_cities,
+        |city| city.monster_attack_risk > limit);
+    println!("{} cities has at risk for monster attack.", n);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
