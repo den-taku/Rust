8be2b6f2cc0977f3ddb7420e88cc7f7f14ff3032
DenTaku <dentaku1412@gmail.com>
2020-08-18 20:22:43 +0900

    ~15.3.4

--- Chapter15/adp/src/main.rs
+++ Chapter15/adp/src/main.rs
@@ -46,6 +46,14 @@ fn main() {
     for element in &iter.collect::<Vec<i32>>() {
         println!("{}", element);
     }
+    let message = "To: jimb\r\n\
+                   From: superego <editor@oreilly.com>\r\n\
+                   \r\n\
+                   Did you get any writing time plotting fractals?\r\n\
+                   When will you stop wasting time plotting fractals?\r\n";
+    for header in message.lines().take_while(|l| !l.is_empty()) {
+        println!("{}", header);
+    }
 }
 
 // fn _hoge(int: i32, float: f32) -> i32 {
