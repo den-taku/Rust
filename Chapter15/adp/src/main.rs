25bcc50221560bb11630c161759c73b60d00e394
DenTaku <dentaku1412@gmail.com>
2020-08-18 22:34:21 +0900

    ~15.3.6

--- Chapter15/adp/src/main.rs
+++ Chapter15/adp/src/main.rs
@@ -1,7 +1,23 @@
 use std::str::FromStr;
 use std::collections::HashMap;
+use std::iter::Peekable;
 
 // fn hoge(int: i32, float: f32) -> i32;
+
+fn parse_number<I>(tokens: &mut Peekable<I>) -> u32
+    where I: Iterator<Item=char>
+{
+    let mut n = 0;
+    loop {
+        match tokens.peek() {
+            Some(r) if r.is_digit(10) => {
+                n = n * 10 + r.to_digit(10).unwrap();
+            }
+            _ => return n
+        }
+        tokens.next();
+    }
+}
 
 fn main() {
     let text = "  ponies  \n    giraffes\niguanas  \nsquid".to_string();
@@ -59,6 +75,12 @@ fn main() {
         .skip(1) {
         println!("{}", body);
     }
+    let mut chars = "226153980,1766319049".chars().peekable();
+    assert_eq!(parse_number(&mut chars), 226153980);
+    // Look, `parse_number` didn't consume the comma! So we will.
+    assert_eq!(chars.next(), Some(','));
+    assert_eq!(parse_number(&mut chars), 1766319049);
+    assert_eq!(chars.next(), None);
 }
 
 // fn _hoge(int: i32, float: f32) -> i32 {
