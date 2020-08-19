916cb93425fe6d5c7e04280b8f465258a0b0444e
DenTaku <dentaku1412@gmail.com>
2020-08-19 13:06:49 +0900

    ~15.4.4

--- Chapter15/consume/src/main.rs
+++ Chapter15/consume/src/main.rs
@@ -1,4 +1,5 @@
 // use std::io::prelude::*;
+use std::cmp::{PartialOrd, Ordering};
 
 fn triangle(n: u64) -> u64 {
     (1..n+1).sum()
@@ -8,10 +9,21 @@ fn factorial(n: u64) -> u64 {
     (1..n+1).product()
 }
 
+// Compare two f64 values. Panic if given a NaN.
+fn cmp(lhs: &&f64, rhs: &&f64) -> Ordering {
+    lhs.partial_cmp(rhs).unwrap()
+}
+
 fn main() {
     // let stdin = std::io::stdin();
     // println!("{}", stdin.lock().lines().count());
     println!("{}", triangle(20));
     println!("{}", factorial(10));
     println!("{}", match [-2, 0, 1, 0, 2, -5].iter().max(){Some(r)=>*r,_=>0});
+
+    let numbers = [1.0, 4.0, 2.0];
+    assert_eq!(numbers.iter().max_by(cmp), Some(&4.0));
+    assert_eq!(numbers.iter().min_by(cmp), Some(&1.0));
+    // let numbers = [1.0, 4.0, std::f64::NAN, 2.0];
+    assert_eq!(numbers.iter().max_by(cmp), Some(&4.0));
 }
