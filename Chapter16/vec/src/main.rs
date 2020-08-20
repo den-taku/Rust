5bd4f9430f9a4e8e685075e9c8535f5560c9ad06
DenTaku <dentaku1412@gmail.com>
2020-08-20 12:36:41 +0900

    ~16.2.0

---
+++ Chapter16/vec/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // Create an empty vector
+    let mut _numbers: Vec<i32> = vec![];
+
+    // Create a vector with given contents
+    let _words = vec!["step", "on", "no", "pets"];
+    let mut _buffer = vec![0u8; 1024]; // 1024 zeroed-out bytes
+    
+    // Create another collection to a vector
+    // let _my_vec = my_set.into_iter().collect::<Vec<String>>();
+}
