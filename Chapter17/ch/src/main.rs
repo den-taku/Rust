a1759fc4b31d3ab9a7a1db0c980811aa94d8e580
DenTaku <dentaku1412@gmail.com>
2020-08-24 22:47:18 +0900

    ~17.2.2

--- Chapter17/ch/src/main.rs
+++ Chapter17/ch/src/main.rs
@@ -2,4 +2,10 @@ fn main() {
     println!("{}", '4'.is_numeric());
     println!("{}", '\n'.is_control());
     println!("{}", ' '.is_whitespace());
+    if let Some(num) = 'a'.to_digit(11) {
+        println!("{}", num);
+    }
+    if let Some(ch) = std::char::from_digit(11, 16) {
+        println!("{}", ch);
+    }
 }
