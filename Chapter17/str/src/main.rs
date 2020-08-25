b2e1b8bb442cfdd2e82b0773c34241486d952b69
DenTaku <dentaku1412@gmail.com>
2020-08-26 02:26:29 +0900

    ~17.3.12

--- Chapter17/str/src/main.rs
+++ Chapter17/str/src/main.rs
@@ -1,6 +1,10 @@
 use std::str::FromStr;
 use std::net::IpAddr;
 
+fn type_of<T>(_: &T) -> String {
+    std::any::type_name::<T>().to_string()
+}
+
 fn main() {
     if let Ok(num) = usize::from_str("3628800") {
         println!("usize: {}", num);
@@ -13,4 +17,15 @@ fn main() {
     assert_eq!(address,
                IpAddr::from([0xfe80, 0, 0, 0, 0x3ea9, 0xf4ff, 0xfe34, 0x7a50]));
     let address = "fe80::0000:03a9:f4ff:fe34:7a50".parse::<IpAddr>().expect("err");
+
+    println!("{}", format!("{}, wow", "doge"));
+    println!("{}", format!("{}", true));
+    println!("{}", format!("({:.3}, {:.3})", 0.5, f64::sqrt(3.0)/2.0));
+
+    let formatted_addr: String = format!("{}", address);
+    println!("{}", formatted_addr);
+    println!("Type of \'formatted\' is {}.", type_of(&formatted_addr));
+    println!("{}", formatted_addr.to_string());
+    let addresses = vec![address, IpAddr::from_str("192.168.0.1").expect("err")];
+    println!("{}", format!("{:?}", addresses));
 }
