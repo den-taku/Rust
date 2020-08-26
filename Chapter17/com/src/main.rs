54f79abb0e64a8e98c69010f308ab8cc4ab42a8f
DenTaku <dentaku1412@gmail.com>
2020-08-27 00:23:06 +0900

    ~17.4.9

--- Chapter17/com/src/main.rs
+++ Chapter17/com/src/main.rs
@@ -1,4 +1,6 @@
 use std::fmt;
+use std::fs::OpenOptions;
+use std::io::Write;
 
 struct Complex { r: f64, i: f64 }
 
@@ -23,6 +25,29 @@ impl fmt::Display for Complex {
     }
 }
 
+fn loggin_enabled() -> bool { true }
+
+fn write_log_entry(entry: std::fmt::Arguments) {
+    if loggin_enabled() {
+        // Keep things simple for now, and just
+        // open the file every time.
+        let mut log_file = OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open("log-file-name")
+            .expect("failed to open log file");
+
+        log_file.write_fmt(entry)
+            .expect("failed to write to log");
+    }
+}
+
+macro_rules! log { // no ! needed ager name in macro deginitions
+    ($format:tt, $($arg:expr), *) => (
+        write_log_entry(format_args!($format, $($arg), *))
+    )
+}
+
 fn main() {
     println!("Hello, world!");
     let one_twenty = Complex { r: -0.5, i: 0.866 };
@@ -34,4 +59,7 @@ fn main() {
     let ninety = Complex { r: 0.0, i: 2.0 };
     println!("{}", ninety);
     println!("{:#}", ninety);
+
+    write_log_entry(format_args!("Hark! {:?}\n", 7893295));
+    log!("0 day and night, but this is wondrous strange! {:?}\n", 3425632);
 }
