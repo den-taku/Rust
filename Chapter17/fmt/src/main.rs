0b855208cb0bd632d24a9753447b3ec4ea8f6b2b
DenTaku <dentaku1412@gmail.com>
2020-08-26 21:44:09 +0900

    ~17.4.5

--- Chapter17/fmt/src/main.rs
+++ Chapter17/fmt/src/main.rs
@@ -1,3 +1,8 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct Complex { r: f64, i: f64 }
+
 fn main() {
     println!("{:.3}μs: relocated {} at {:#x} to {:#x}, {} bytes",
         0.84391, "object",
@@ -6,4 +11,12 @@ fn main() {
     println!("{:*<4}", "th\u{e9}");
     println!("{:*<4}", "th\u{301}");
     println!("{:=^12}", 1234);
+
+    let mut map = HashMap::new();
+    map.insert("Portland", (45.5237606,-122.6819273));
+    map.insert("Taipei", (25.0375167,121.5637));
+    println!("{:#?}", map);
+
+    let third = Complex{ r: -0.5, i: f64::sqrt(0.75) };
+    println!("{:?}", third);
 }
