9a896701627922858726c4380fb4635ad2310bd9
DenTaku <dentaku1412@gmail.com>
2020-08-26 17:48:56 +0900

    ~17.4.0

---
+++ Chapter17/fmt/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("{:.3}μs: relocated {} at {:#x} to {:#x}, {} bytes",
+        0.84391, "object",
+        140737488346304_usize, 6299664_usize, 64);
+    println!("{{a, c}} ⊂ {{a, b, c}}");
+}
