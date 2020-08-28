97a0b709d9de27846ac777128e43ae61b6d4b002
DenTaku <dentaku1412@gmail.com>
2020-08-28 23:56:53 +0900

    ~18.1.0

---
+++ Chapter18/stdtrait/src/main.rs
@@ -0,0 +1,26 @@
+use std::io::{self, Read, Write, ErrorKind};
+
+const DEFAULT_BUF_SIZE: usize = 8 * 1024;
+
+pub fn copy<R: ?Sized, W: ?Sized>(reader: &mut R, writer: &mut W)
+    -> io::Result<u64>
+    where R: Read, W: Write
+{
+    let mut buf = [0; DEFAULT_BUF_SIZE];
+    let mut written = 0;
+    loop {
+        let len = match reader.read(&mut buf) {
+            Ok(0) => return Ok(written),
+            Ok(len) => len,
+            Err(ref e) if e.kind() == ErrorKind::Interrupted => continue,
+            Err(e) => return Err(e),
+        };
+        writer.write_all(&buf[..len])?;
+        written += len as u64;
+    }
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}
