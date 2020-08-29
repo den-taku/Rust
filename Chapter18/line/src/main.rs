00734bdc9f71a46e82049608eb3a5c1bc3a8f3c9
DenTaku <dentaku1412@gmail.com>
2020-08-29 22:22:20 +0900

    ~18.1.3

---
+++ Chapter18/line/src/main.rs
@@ -0,0 +1,25 @@
+use std::io;
+use std::io::prelude::*;
+
+fn grep<R>(target: &str, reader: R) -> io::Result<()> 
+    where R: BufRead
+{
+    for line_result in reader.lines() {
+        let line = line_result?;
+        if line.contains(target) {
+            println!("{}", line);
+        }
+    }
+    Ok(())
+}
+
+
+fn main() {
+    let target = "hoge"; 
+
+    let stdin = io::stdin();
+    grep(&target, stdin.lock()).expect("err");
+
+    // let f = File::open(file).expect("err");
+    // grep(&target, BufReader::new(f)).expect("err");
+}
