3e4c8c4ea3743099a95d1ec0a63af99ae3fd58e9
DenTaku <dentaku1412@gmail.com>
2020-08-31 23:49:19 +0900

    ~19.1.2

--- Chapter19/fj/src/main.rs
+++ Chapter19/fj/src/main.rs
@@ -1,3 +1,29 @@
+use std::thread::spawn;
+use std::io;
+
+fn process_files_in_parallel(filename: Vec<String>) -> io::Result<()> {
+    // Divede the work into several chunks.
+    const NTHREADS: usize = 8;
+    let worklists = split_vec_into_chunks(filename, NTHREADS);
+
+    // Fork: Spawn a thread to handle each chunk.
+    let mut thread_handles = vec![];
+    for worklist in worklists {
+        thread_handles.push(
+            spawn(move || process_files(worklist))
+        );
+    }
+
+    // Join: Wait for all threads to finish.
+    for handle in thread_handles {
+        handle.join().unwrap()?;
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    spawn(|| {
+        println!("Hello, world!");
+    });
 }
