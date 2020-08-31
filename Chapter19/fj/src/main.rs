cf1e7a7a2384e6aea3acba069adee180e58ec833
DenTaku <dentaku1412@gmail.com>
2020-09-01 01:55:56 +0900

    ~19.1.3

--- Chapter19/fj/src/main.rs
+++ Chapter19/fj/src/main.rs
@@ -1,7 +1,14 @@
 use std::thread::spawn;
 use std::io;
+use std::collections::HashMap;
+use std::sync::Arc;
 
-fn process_files_in_parallel(filename: Vec<String>) -> io::Result<()> {
+type GigabyteMap = HashMap<char, String>;
+
+fn process_files_in_parallel(filename: Vec<String>,
+                             glossary: Arc<GigabyteMap>)
+    -> io::Result<()> 
+{
     // Divede the work into several chunks.
     const NTHREADS: usize = 8;
     let worklists = split_vec_into_chunks(filename, NTHREADS);
@@ -9,8 +16,11 @@ fn process_files_in_parallel(filename: Vec<String>) -> io::Result<()> {
     // Fork: Spawn a thread to handle each chunk.
     let mut thread_handles = vec![];
     for worklist in worklists {
+        // This call to .clone() only clones the Arc and bumps the 
+        // reference count. It does not clone the GigabyteMap.
+        let glossary_for_child = glossary.clone();
         thread_handles.push(
-            spawn(move || process_files(worklist))
+            spawn(move || process_files(worklist, &glossary_for_child))
         );
     }
 
