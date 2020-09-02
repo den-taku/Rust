ebc60f02a9cfcf34d3311eee2180fc88faf2ad0e
DenTaku <dentaku1412@gmail.com>
2020-09-03 05:09:59 +0900

    ~20.1.0

--- Chapter20/ass/src/main.rs
+++ Chapter20/ass/src/main.rs
@@ -1,4 +1,21 @@
+macro_rules! assert_eq2 {
+    ($left:expr, $right:expr) => ({
+        match (&$left, &$right) {
+            (left_var, right_var) => {
+                if !(*left_var == *right_var) {
+                    panic!("assertion failed: `(left == right)` \
+                            (left: `{:?}`, right: `{:?}`)",
+                            left_var, right_var)
+                }
+            }
+        }
+    });
+}
+
+/*hoge*/
+
 fn main() {
+    assert_eq2!(1,2);
     assert_eq!(1,2);
     println!("Hello, world!");
 }
