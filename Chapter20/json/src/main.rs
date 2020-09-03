83aa21d5325e01beabad95c02aa369925a580023
DenTaku <dentaku1412@gmail.com>
2020-09-03 19:31:44 +0900

    ~20.4.2

--- Chapter20/json/src/main.rs
+++ Chapter20/json/src/main.rs
@@ -1,14 +1,18 @@
 use std::collections::HashMap;
 
+// #![recursion_limit = "256"]
+
 macro_rules! json {
     (null) => {
         Json::Null
     };
     ([ $( $element:tt ),* ]) => {
-        Json::Array(vec![ $( $element ),* ])
+        Json::Array(vec![ $( json!($element) ),* ])
     };
     ({ $( $key:tt : $value:tt ),* }) => {
-        Json::Object(Box::new((vec![ ( $( $key , $value ),* ) ]).iter().collect::<HashMap<_,_>>()))
+        Json::Object(Box::new(vec![
+            $( ($key.to_string(), json!($value)) ),*
+        ].into_iter().collect()))
     };
 }
 
@@ -31,7 +35,7 @@ fn json_array_with_json_element() {
         Json::Array(vec![
             Json::Object(Box::new(vec![
                 ("pitch".to_string(), Json::Number(440.0))
-            ].collect()))
+            ].into_iter().collect()))
         ]);
     assert_eq!(macro_generated_value, hand_coded_value);
 }
