d184a30de42bdd38d9a09d5207dc72f2974d657c
DenTaku <dentaku1412@gmail.com>
2020-09-08 16:15:32 +0900

    Review Chapter12

--- Review/index/src/main.rs
+++ Review/index/src/main.rs
@@ -2,4 +2,8 @@ fn main() {
     let mut array = [0,1,2,3,4,5,6,7,8,9];
     array[2] = 20;
     println!("{:#?}", array);
+    let mut a = 10;
+    let mut b = &a;
+    b = &20;
+    println!("{}", a);
 }
